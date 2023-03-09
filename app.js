379cea1b19ead3ab8c3595dd5ea753330a7f3d7b
lalit kawadker <lalit.kawadker@freighttiger.com>
2023-03-09 18:54:41 +0530

    added new build

---
+++ app.js
@@ -0,0 +1,28 @@
+// require('dotenv').config({ path: '.env' })
+const express = require('express');
+const path = require('path')
+
+
+const router = express.Router();
+const app = express();
+const cors = require('cors');
+app.use(express.json({ limit: '50mb' }));
+app.use(cors({
+    origin: ['*', 'http://localhost:5000']
+}));
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static(path.join(__dirname, 'dist/angular-app')));
+
+app.use('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/angular-app/index.html'))
+});
+
+app.get('/test-app', (req, res) => {
+    res.status(200).json({ data: 'server working fine ', success: true }).end();
+})
+app.listen(5000, () => {
+    console.log(`server started on port:-3000`)
+});
